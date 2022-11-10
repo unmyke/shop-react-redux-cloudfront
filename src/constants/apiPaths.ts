37d27c70a6fd55235a7bb609fad8d8c2163a95b2
Mikhail Unenov <mikhail_unenov@epam.com>
2022-11-10 22:52:55 +0400

    chore(deps): add import service endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://9w18vvojii.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://9w18vvojii.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://9w18vvojii.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://cjkdzl1oa2.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://9w18vvojii.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://9w18vvojii.execute-api.eu-west-1.amazonaws.com/dev",
 };
